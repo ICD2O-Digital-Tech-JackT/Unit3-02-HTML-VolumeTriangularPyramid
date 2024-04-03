668cb24b36fe4fa5cbcb8d5b508bfc004abcd5c6
Jack Turcotte <159164296+JackTurcs@users.noreply.github.com>
2024-04-03 19:55:31 +0000

    Finished

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+function Display(Volume){
+  if(Volume==0){
+     document.getElementById("Output").innerHTML="Please enter valid numbers"
+  } else{
+     document.getElementById("Output").innerHTML="The area of this pyramid is: " + Volume.toFixed(2);
+  } 
+}
+
+function Calculate(){
+  let Height = document.getElementById("Height").value;
+  let SideA = document.getElementById("SideA").value;
+  let SideB = document.getElementById("SideB").value;
+  let Volume = (1/6)*SideA*SideB*Height
+  Display(Volume);
+}
